c2984ce3d7f2dfb3a5bb0d5ad18daf07f38be3fb
Yuseif <yuseifabdillah@gmail.com>
2022-12-10 21:11:03 +0300

    feat: Add site title to django admin panel

--- pineapple_api/urls.py
+++ pineapple_api/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+admin.site.site_header = 'PineApple'
+admin.site.site_title = 'PineApple'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
