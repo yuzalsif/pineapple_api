c3b70786a106f4a94a0cc8e095e98bf9e42f5ffa
Yuseif <yuseifabdillah@gmail.com>
2022-12-13 12:01:30 +0300

    feat: Add Cart Items column in the admin panel
    This add Cart Items column for Cart in the admin panel, when clicked
    link to cart items of the cart is enabled.

--- shop/admin.py
+++ shop/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.http import urlencode
+from django.urls import reverse
+from django.db.models import Count
 
 from .models import *
 
@@ -40,8 +44,22 @@ class OrderItemAdmin(admin.ModelAdmin):
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    list_display = ['placed_at', 'customer']
+    list_display = ['placed_at', 'customer', 'show_cart_items_of_this_cart']
     list_per_page = number_of_items_per_page
+
+    @admin.display(description='Cart Items')
+    def show_cart_items_of_this_cart(self, cart):
+        url = (
+            reverse('admin:shop_cartitem_changelist') + '?' +
+            urlencode({
+                'cart__id': str(cart.pk)
+            }))
+        return format_html("<a href='{}'>{}<a/>", url, cart.cart_items_count)
+
+    def get_queryset(self, request) :
+        return super().get_queryset(request).annotate(
+            cart_items_count = Count('cartitem')
+        )
 
 @admin.register(CartItem)
 class CartItemAdmin(admin.ModelAdmin):
