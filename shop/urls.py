cd492c68f6230ed7e05530dbfc3119c64c558368
Yuseif <yuseifabdillah@gmail.com>
2022-12-19 09:35:07 +0300

    feat: Add shop detail views

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('shop/', views.get_all_shops, name='shops')
+    path('shop/', views.get_all_shops, name='shops'),
+    path('shop/<int:id>', views.get_shop_detail, name='shops_details')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import *
+from .serializers import *
+
+@api_view()
+def get_all_shops(request):
+    queryset = Shop.objects.all()
+    serializer = ShopSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+@api_view()
+def get_shop_detail(request, id):
+    shop = get_object_or_404(Shop, pk=id)
+    serializer = ShopSerializer(shop)
+    return Response(serializer.data)
