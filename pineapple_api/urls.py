d0a98b72acb571348c2b7d755c0d6fe4e3cc3d05
Yuseif <yuseifabdillah@gmail.com>
2022-12-19 06:36:55 +0300

    chore: Add restful framework to path and settings
    This add django retsful framework app to settings.py and add it's
    basic urls path to urls.py

--- pineapple_api/urls.py
+++ pineapple_api/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 admin.site.site_header = 'PineApple'
@@ -22,4 +22,5 @@ admin.site.site_title = 'PineApple'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls'))
 ]
